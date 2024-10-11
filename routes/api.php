870d28e4d9328175f486d69e1900af933e07b5db
Jade Geels <jade@justbetter.nl>
2024-10-11 14:46:58 +0200

    Add web middleware to check csrf token

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,5 @@
 use Rapidez\VatValidation\Http\Controllers\VatController;
 
 Route::middleware('api')->prefix('api')->group(function () {
-    Route::post('vat-validate', VatController::class);
+    Route::post('vat-validate', VatController::class)->middleware('web');
 });
