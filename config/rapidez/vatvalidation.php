a483e47be5f9941648559810b708d29a29d8942d
Jade Geels <jade@justbetter.nl>
2025-09-24 15:50:06 +0200

    Forced validation option (#10)

--- config/rapidez/vatvalidation.php
+++ config/rapidez/vatvalidation.php
@@ -10,4 +10,15 @@ return [
     // - When a guest cart is active.
     // Enable this setting to allow the validation to be used anywhere.
     'allow_anywhere' => false,
+
+    // Should we allow non-VIES-validateable countries to skip validation?
+    // We can only validate VAT numbers that come from EU countries. This means that anything that doesn't come from here will get skipped by default.
+    // If you want to force validation on everything that's written into the VAT input, enable this option.
+    'force_validation' => false,
+
+    // Depending on your customer base, you might want to allow certain non-EU countries through without being validated through VIES.
+    // A good example of this would be VAT numbers starting with `GB`. These may be valid but can't be validated through the VIES API.
+    'force_exclusions' => [
+        // 'GB',
+    ],
 ];
--- src/Http/ViewComposers/ConfigComposer.php
+++ src/Http/ViewComposers/ConfigComposer.php
@@ -9,6 +9,10 @@ class ConfigComposer
 {
     public function compose(View $view)
     {
-        Config::set('frontend.vat_validation.translations', __('rapidez-vat::frontend.vat_validation'));
+        Config::set('frontend.vat_validation', [
+            'force_validation' => config('rapidez.vatvalidation.force_validation'),
+            'force_exclusions' => config('rapidez.vatvalidation.force_exclusions'),
+            'translations' => __('rapidez-vat::frontend.vat_validation'),
+        ]);
     }
 }
