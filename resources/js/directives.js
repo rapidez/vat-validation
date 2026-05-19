c4790ea47f9ec6937c8428e59ff0efc91ade8372
Jade Geels <jade@justbetter.nl>
2026-05-19 08:41:51 +0000

    FIx vue3 compatibility (#15)

--- resources/js/directives.js
+++ resources/js/directives.js
@@ -1,11 +1,14 @@
 import validateElementVAT from './vat-validation'
 import { useEventListener } from '@vueuse/core'
 
-Vue.directive('validate', {
-    bind(el, binding) {
-        if ('vat' in binding.modifiers) {
-            useEventListener(el, 'change', (event) => validateElementVAT(event.target))
-            validateElementVAT(el)
+document.addEventListener('vue:loaded', function (event) {
+    const vue = event.detail.vue
+    vue.directive('validate', {
+        bind(el, binding) {
+            if ('vat' in binding.modifiers) {
+                useEventListener(el, 'change', (event) => validateElementVAT(event.target))
+                validateElementVAT(el)
+            }
         }
-    }
+    })
 })
