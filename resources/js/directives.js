74b6efd03bd7a50616843865b94c95ee44a87993
Jade Geels <jade@justbetter.nl>
2026-07-10 06:16:14 +0000

    Vue 3 compatibility fix (#16)

--- resources/js/directives.js
+++ resources/js/directives.js
@@ -4,7 +4,7 @@ import { useEventListener } from '@vueuse/core'
 document.addEventListener('vue:loaded', function (event) {
     const vue = event.detail.vue
     vue.directive('validate', {
-        bind(el, binding) {
+        mounted(el, binding) {
             if ('vat' in binding.modifiers) {
                 useEventListener(el, 'change', (event) => validateElementVAT(event.target))
                 validateElementVAT(el)
