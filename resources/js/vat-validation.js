73d1d5167ff0d454775e9b54ad94599474db9d76
Jade Geels <jade@justbetter.nl>
2026-09-08 06:50:56 +0000

    Use jsvat-next (#19)

--- resources/js/vat-validation.js
+++ resources/js/vat-validation.js
@@ -1,7 +1,7 @@
 import { useThrottleFn, useMemoize } from '@vueuse/core'
 import { token } from 'Vendor/rapidez/core/resources/js/stores/useUser'
 import { mask } from 'Vendor/rapidez/core/resources/js/stores/useMask'
-import { checkVAT, countries } from 'jsvat'
+import { checkVAT, countries } from 'jsvat-next'
 
 const viesCheckable = ['AT', 'BE', 'BG', 'CY', 'CZ', 'DE', 'DK', 'EE', 'EL', 'ES', 'FI', 'FR', 'HR', 'HU', 'IE', 'IT', 'LT', 'LU', 'LV', 'MT', 'NL', 'PL', 'PT', 'RO', 'SE', 'SI', 'SK', 'XI']
 
